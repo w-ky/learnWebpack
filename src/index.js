228654ea9ce20db6b275025ad67f198b28f763c6
kyan <kyan@w.cn>
2020-12-10 15:58:37 +0800

    '增加二维码插件'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("success")
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import QRCode from "qrcode.react";//生成二维码插件
+
+class Wrap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+  }
+  handleChange(e) {
+    this.setState({
+      value : e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.value} onChange={(e) => this.handleChange(e)}/>
+        <QRCode value={this.state.value} size={200} />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Wrap />, document.getElementById("root"));
